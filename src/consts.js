62c172dbfdb054e78367af82b6fa4824c51782b7
Miguel Amores <migueamores@gmail.com>
2025-09-01 07:30:35 -0500

    add new data

--- src/consts.js
+++ src/consts.js
@@ -1,17 +1,13 @@
+import Danzante from "../src/assets/danzante.png";
+
 export const cardList = [
   {
     id: "waverley",
     title: "Waverley - Kimedics",
     content:
       "Worked as Frontend developer in the Helth Care app built in react using vite. Created complex forms with dynamic validations",
-    details: [
-      "Migrated an old code base to a new one using the latest technologies.",
-      "Created complex forms with dynamic validations using reusable and tested components with react-hook-form and zod.",
-      "Created entire pages showing lists, ability to create and clone items, sorting, filtering, and exporting data, allowing the user access to specific items within the system.",
-      "Collaborated closely with the QA team, created unit and integration testing to ensure code quality using vitest and react-testing-library.",
-      "Performed QA testing before production launch using CI/CD.",
-      "Managed a design system of reusable components using Storybook.",
-    ],
+    details: "../src/assets/danzante.png",
+    img: "../src/assets/danzante.png",
     tags: [
       "React",
       "Typescript",
@@ -26,16 +22,8 @@ export const cardList = [
     title: "Image Wizard",
     content:
       "Created a SaaS web application to transform and generate images using IA",
-    details: [
-      "Created an AI image editor and generator from scratch using Cloudinary and the fal-ai model.",
-      "Handled user authentication using Clerk. Assigned limited free credits to each user, integrating Strapi as a payment method.",
-      "Connected to a MongoDB through NextJS server actions to save modifications inside the database.",
-      "Created server components for better SEO and faster page load times.",
-      "Used accurate HTML tags to provide better SEO advantages for crawlers.",
-      "Created metadata for each page to improve SEO.",
-      "Created reusable and tested components using Shadcn, Tailwindcss and Vitest, react-testing-library, and playwright for testing.",
-      "Implemented CI/CD with GitHub Actions for fast and secure deployments.",
-    ],
+    details: "../src/assets/danzante.png",
+    img: "../src/assets/danzante.png",
     tags: [
       "Nextjs",
       "React",
@@ -52,6 +40,88 @@ export const cardList = [
     title: "Genome",
     content:
       "Worked as Frontend and Mobile developer across many applications delivering features",
+    details: "../src/assets/danzante.png",
+    img: "../src/assets/danzante.png",
+    tags: [
+      "React",
+      "React Native",
+      "Typescript",
+      "Styled Components",
+      "Tailwindcss",
+    ],
+  },
+];
+
+export const references = [
+  {
+    id: "waverley",
+    title: "EL DANZANTE DE PUJILÍ",
+    content:
+      "En el diseño, la figura del Danzante de Pujilí adquiere especial relevancia al inspirar la incorporación de la forma circular como representación del sol. Este elemento, presente en la propia vestimenta del danzante, no solo refuerza el simbolismo de energía, vitalidad y centralidad en la cosmovisión andina, sino que también sirve como concepto articulador en la propuesta de diseño, evocando la unión entre lo festivo, lo espiritual y la naturaleza.",
+    details: "../src/assets/danzante-2.png",
+    img: "../src/assets/danzante.png",
+    tags: ["Danzante", "Pujilí", "Sinchahuasín", "Sol"],
+  },
+  {
+    id: "image-wizard",
+    title: "CRUZ CHAKANA",
+    content:
+      "El uso de la cruz chakana en el diseño enriquece la experiencia del visitante: su forma escalonada orienta el recorrido, marca ritmos de atención y facilita una navegación intuitiva entre el museo, los talleres y los puntos de venta, prolongando la permanencia y elevando la calidad de la vivencia. Al mismo tiempo, potencia la legitimidad cultural del proyecto: al vincularse con un símbolo ancestral, se refuerza la percepción de autenticidad, respeto y diálogo entre tradición y modernidad, lo que contribuye a atraer tanto a comunidades locales como a turistas interesados en las identidades culturales.",
+    details: "../src/assets/danzante.png",
+    img: "../src/assets/danzante.png",
+    tags: ["Rojo", "Amarillo", "Verde", "Azul", "Sabiduría"],
+  },
+  {
+    id: "genome",
+    title: "GRADAS DEL SINCHAHUASÍN",
+    content:
+      "El conjunto de gradas del Sinchahuasín es un elemento fundamental en el paisaje urbano y cultural del cantón Pujilí. Este sistema, compuesto por aproximadamente 600 escalones, asciende hasta la cima de la colina del mismo nombre, formada naturalmente por procesos geológicos de acumulación de tierra y roca. La construcción de estas gradas se originó para facilitar el acceso peatonal a esta elevación, que ha sido históricamente un mirador y un lugar simbólico de encuentro.",
+    details: "../src/assets/danzante.png",
+    img: "../src/assets/danzante.png",
+    tags: ["Gradas", "Montaña", "Mirador", "Danzante"],
+  },
+];
+
+export const prototypes = [
+  {
+    id: "waverley",
+    title: "Prototipo 1",
+    content:
+      "La maqueta elaborada plantea un prototipo con acabadoblanco, natural y tranquilo. El diseño contempla una cúpula central que permite un ingreso regulado de luz natural, con el propósito de generar una atmósfera interior adecuada para actividades de exposición y presentación, sin comprometer la calidad visual. Asimismo, se incorporan filtros de color en las paredes, con el fin de introducir diferentes gamas lumínicas que transforman la percepción del espacio y aportan una experiencia sensorial dinámica y envolvente.",
+    details: [
+      "Migrated an old code base to a new one using the latest technologies.",
+      "Created complex forms with dynamic validations using reusable and tested components with react-hook-form and zod.",
+      "Created entire pages showing lists, ability to create and clone items, sorting, filtering, and exporting data, allowing the user access to specific items within the system.",
+      "Collaborated closely with the QA team, created unit and integration testing to ensure code quality using vitest and react-testing-library.",
+      "Performed QA testing before production launch using CI/CD.",
+      "Managed a design system of reusable components using Storybook.",
+    ],
+    img: "../src/assets/prototipo-1.png",
+    tags: ["Paper Craft", "Maqueta"],
+  },
+  {
+    id: "image-wizard",
+    title: "Prototipo 2",
+    content:
+      "En este caso se desarrolló una maqueta con un prototipo de acabado reflectante, concebido como un elemento integrador entre el equipamiento arquitectónico y el cielo, en concordancia con su ubicación en la cima del cerro Sicnhahuasín. La propuesta incorpora una cúpula central cuya configuración busca generar un juego controlado de entradas de luz natural. Este recurso lumínico se plantea de manera que potencie la atmósfera interior sin interferir con las actividades de presentación o exposición que se desarrollen en el espacio, garantizando condiciones óptimas de confort visual y de experiencia espacial.",
+    details: [
+      "Created an AI image editor and generator from scratch using Cloudinary and the fal-ai model.",
+      "Handled user authentication using Clerk. Assigned limited free credits to each user, integrating Strapi as a payment method.",
+      "Connected to a MongoDB through NextJS server actions to save modifications inside the database.",
+      "Created server components for better SEO and faster page load times.",
+      "Used accurate HTML tags to provide better SEO advantages for crawlers.",
+      "Created metadata for each page to improve SEO.",
+      "Created reusable and tested components using Shadcn, Tailwindcss and Vitest, react-testing-library, and playwright for testing.",
+      "Implemented CI/CD with GitHub Actions for fast and secure deployments.",
+    ],
+    img: "../src/assets/prototipo-2.png",
+    tags: ["Juego de luces", "Reflectividad"],
+  },
+  {
+    id: "genome",
+    title: "Prototipo 3",
+    content:
+      "La maqueta presenta un diseño innovador que utiliza cúpulas para maximizar la entrada de luz natural y permitir la exhibición del cielo, creando un ambiente luminoso y atractivo. Las cúpulas rojas, además de ser estéticamente agradables, sirven como espacios versátiles para actividades y exposiciones, fomentando la interacción con el entorno.",
     details: [
       `Worked on the Beyond Meat e-commerce project, building the site from scratch in a team of 3
 developers. We created an entire design system in React and TypeScript, integrated with Directus CMS
@@ -75,44 +145,7 @@ implement a detail page for users who connect with free accounts. Handled deploy
 TestFlight.`,
       `Worked at Opal, creating new product detail pages in a Shopify e-commerce using Liquid.`,
     ],
-    tags: [
-      "React",
-      "React Native",
-      "Typescript",
-      "Styled Components",
-      "Tailwindcss",
-    ],
-  },
-  {
-    id: "verndale",
-    title: "Verndale",
-    content: "Re designed an entire web application into a completely new one",
-    details: [
-      "Redesigned the entire WireMasters e-commerce frontend based on a Figma design. Applied modern CSS techniques to simplify mobile and desktop views. Adding new features using vanilla JavaScript.",
-    ],
-    tags: ["React", "Javascript", "CSS"],
-  },
-  {
-    id: "trade",
-    title: "Trade",
-    content:
-      "Worked as a Frontend and Mobile developer across many applications",
-    details: [
-      "Integrated the Juan Valdez mobile with Firebase to show user points in real time. Created a QR modal that allows users to reclaim products at the store.",
-      "Led the development of Duragas mobile and web apps in a team of 4 engineers. Integrate a component library across the web and mobile apps. Integrated with a REST API to schedule gas delivery.",
-      "Worked at Omedym multi-tenant system to improve code readability. Implemented state management at the client side, refactored logic into smaller functions. We produced a more scalable and maintainable code. Removed bad practices from CSS and applied CSS Grid to achieve a better layout across mobile and desktop views.",
-    ],
-    tags: ["React", "React Native", "Javascript", "Styled Components"],
-  },
-  {
-    id: "ndeveloper",
-    title: "nDeveloper",
-    content: "Worked as backend developer maintaining and creating APIs",
-    details: [
-      "Worked in Bancolombia, migrating raw SQL queries into Hibernate ORM.",
-      "Worked in a Courier app, creating the MVC backend in Java and connecting to an Oracle SQL. Created the services using the TDD approach. Created the frontend app using AngularJS and connected to the Java endpoints.",
-      "Worked at UTE, creating endpoints that retrieve students’ information across a university.",
-    ],
-    tags: ["Java", "Javascript", "Nodejs"],
+    img: "../src/assets/prototipo-3.png",
+    tags: ["Natural", "Cúpula cerrada"],
   },
 ];
