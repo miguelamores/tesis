d8bd126686dc118de0e19a1f8f857bf3c1c22715
Miguel Amores <migueamores@gmail.com>
2025-09-02 09:04:01 -0500

    get all images from public

--- src/consts.js
+++ src/consts.js
@@ -1,65 +1,11 @@
-import Danzante from "../src/assets/danzante.png";
-
-export const cardList = [
-  {
-    id: "waverley",
-    title: "Waverley - Kimedics",
-    content:
-      "Worked as Frontend developer in the Helth Care app built in react using vite. Created complex forms with dynamic validations",
-    details: "../src/assets/danzante.png",
-    img: "../src/assets/danzante.png",
-    tags: [
-      "React",
-      "Typescript",
-      "Tailwindcss",
-      "Vitest",
-      "React Testing Library",
-      "Testing",
-    ],
-  },
-  {
-    id: "image-wizard",
-    title: "Image Wizard",
-    content:
-      "Created a SaaS web application to transform and generate images using IA",
-    details: "../src/assets/danzante.png",
-    img: "../src/assets/danzante.png",
-    tags: [
-      "Nextjs",
-      "React",
-      "Typescript",
-      "Tailwindcss",
-      "Cloudinary",
-      "Fal-AI",
-      "MongoDB",
-      "Stripe",
-    ],
-  },
-  {
-    id: "genome",
-    title: "Genome",
-    content:
-      "Worked as Frontend and Mobile developer across many applications delivering features",
-    details: "../src/assets/danzante.png",
-    img: "../src/assets/danzante.png",
-    tags: [
-      "React",
-      "React Native",
-      "Typescript",
-      "Styled Components",
-      "Tailwindcss",
-    ],
-  },
-];
-
 export const references = [
   {
     id: "waverley",
     title: "EL DANZANTE DE PUJILÍ",
     content:
       "En el diseño, la figura del Danzante de Pujilí adquiere especial relevancia al inspirar la incorporación de la forma circular como representación del sol. Este elemento, presente en la propia vestimenta del danzante, no solo refuerza el simbolismo de energía, vitalidad y centralidad en la cosmovisión andina, sino que también sirve como concepto articulador en la propuesta de diseño, evocando la unión entre lo festivo, lo espiritual y la naturaleza.",
-    details: "../src/assets/danzante-2.png",
-    img: "../src/assets/danzante.png",
+    details: "/images/danzante-2.png",
+    img: "/images/danzante.png",
     tags: ["Danzante", "Pujilí", "Sinchahuasín", "Sol"],
   },
   {
@@ -67,7 +13,7 @@ export const references = [
     title: "CRUZ CHAKANA",
     content:
       "El uso de la cruz chakana en el diseño enriquece la experiencia del visitante: su forma escalonada orienta el recorrido, marca ritmos de atención y facilita una navegación intuitiva entre el museo, los talleres y los puntos de venta, prolongando la permanencia y elevando la calidad de la vivencia. Al mismo tiempo, potencia la legitimidad cultural del proyecto: al vincularse con un símbolo ancestral, se refuerza la percepción de autenticidad, respeto y diálogo entre tradición y modernidad, lo que contribuye a atraer tanto a comunidades locales como a turistas interesados en las identidades culturales.",
-    details: "../src/assets/cruz-detail.png",
+    details: "/images/cruz-detail.png",
     img: "/images/cruz.png",
     tags: ["Rojo", "Amarillo", "Verde", "Azul", "Sabiduría"],
   },
@@ -76,8 +22,8 @@ export const references = [
     title: "GRADAS DEL SINCHAHUASÍN",
     content:
       "El conjunto de gradas del Sinchahuasín es un elemento fundamental en el paisaje urbano y cultural del cantón Pujilí. Este sistema, compuesto por aproximadamente 600 escalones, asciende hasta la cima de la colina del mismo nombre, formada naturalmente por procesos geológicos de acumulación de tierra y roca. La construcción de estas gradas se originó para facilitar el acceso peatonal a esta elevación, que ha sido históricamente un mirador y un lugar simbólico de encuentro.",
-    details: "../src/assets/gradas-detail.png",
-    img: "../src/assets/gradas.png",
+    details: "/images/gradas-detail.png",
+    img: "/images/gradas.png",
     tags: ["Gradas", "Montaña", "Mirador", "Danzante"],
   },
 ];
@@ -88,15 +34,8 @@ export const prototypes = [
     title: "Prototipo 1",
     content:
       "La maqueta elaborada plantea un prototipo con acabadoblanco, natural y tranquilo. El diseño contempla una cúpula central que permite un ingreso regulado de luz natural, con el propósito de generar una atmósfera interior adecuada para actividades de exposición y presentación, sin comprometer la calidad visual. Asimismo, se incorporan filtros de color en las paredes, con el fin de introducir diferentes gamas lumínicas que transforman la percepción del espacio y aportan una experiencia sensorial dinámica y envolvente.",
-    details: [
-      "Migrated an old code base to a new one using the latest technologies.",
-      "Created complex forms with dynamic validations using reusable and tested components with react-hook-form and zod.",
-      "Created entire pages showing lists, ability to create and clone items, sorting, filtering, and exporting data, allowing the user access to specific items within the system.",
-      "Collaborated closely with the QA team, created unit and integration testing to ensure code quality using vitest and react-testing-library.",
-      "Performed QA testing before production launch using CI/CD.",
-      "Managed a design system of reusable components using Storybook.",
-    ],
-    img: "../src/assets/prototipo-1.png",
+    details: "",
+    img: "/images/prototipo-1.png",
     tags: ["Paper Craft", "Maqueta"],
   },
   {
@@ -104,17 +43,8 @@ export const prototypes = [
     title: "Prototipo 2",
     content:
       "En este caso se desarrolló una maqueta con un prototipo de acabado reflectante, concebido como un elemento integrador entre el equipamiento arquitectónico y el cielo, en concordancia con su ubicación en la cima del cerro Sicnhahuasín. La propuesta incorpora una cúpula central cuya configuración busca generar un juego controlado de entradas de luz natural. Este recurso lumínico se plantea de manera que potencie la atmósfera interior sin interferir con las actividades de presentación o exposición que se desarrollen en el espacio, garantizando condiciones óptimas de confort visual y de experiencia espacial.",
-    details: [
-      "Created an AI image editor and generator from scratch using Cloudinary and the fal-ai model.",
-      "Handled user authentication using Clerk. Assigned limited free credits to each user, integrating Strapi as a payment method.",
-      "Connected to a MongoDB through NextJS server actions to save modifications inside the database.",
-      "Created server components for better SEO and faster page load times.",
-      "Used accurate HTML tags to provide better SEO advantages for crawlers.",
-      "Created metadata for each page to improve SEO.",
-      "Created reusable and tested components using Shadcn, Tailwindcss and Vitest, react-testing-library, and playwright for testing.",
-      "Implemented CI/CD with GitHub Actions for fast and secure deployments.",
-    ],
-    img: "../src/assets/prototipo-2.png",
+    details: "",
+    img: "/images/prototipo-2.png",
     tags: ["Juego de luces", "Reflectividad"],
   },
   {
@@ -122,30 +52,8 @@ export const prototypes = [
     title: "Prototipo 3",
     content:
       "La maqueta presenta un diseño innovador que utiliza cúpulas para maximizar la entrada de luz natural y permitir la exhibición del cielo, creando un ambiente luminoso y atractivo. Las cúpulas rojas, además de ser estéticamente agradables, sirven como espacios versátiles para actividades y exposiciones, fomentando la interacción con el entorno.",
-    details: [
-      `Worked on the Beyond Meat e-commerce project, building the site from scratch in a team of 3
-developers. We created an entire design system in React and TypeScript, integrated with Directus CMS
-through a GraphQL endpoint, and generated the static site at build time for better SEO. We connected
-the site to the Shopify cart and checkout. Shipped the site supporting multiple locations and languages.`,
-      `Worked on the iRobot mobile app built in React Native. I upgraded the framework to the latest one and
-refactored the app from class-based to function-based components, refactored them, and separated logic
-into custom hooks. Reduced the complexity of adding new features by extracting logic and creating unit
-and integration testing. Redesigned the entire UI, collaborating with the design team.`,
-      `Worked on the Clear Choice health care mobile app, creating the app from scratch. Created reusable and
-tested components and integrated them into a Contentful CMS to get the data. Connected to the CMS
-through graphql to get detail information and generate pages. Integrated REST API endpoints to manage
-user appointments. Led the development in a team of 3 engineers.`,
-      `Worked at Keebler to implement a Christmas feature that let users create an AR house. Implemented the
-React logic to build a house using a gallery of images. Integrated AFrame to achieve the AR experience.`,
-      `Worked at PMA, fixing a deployment issue related to node version. Upgraded Nodejs and multiple library
-versions to allow the deployment of new features.`,
-      `Worked at Billboard Hot 100 mobile app. Optimized the authenticated flow by upgrading react-
-navigation and fixing known bugs from the previous version. Collaborated with the Design team to
-implement a detail page for users who connect with free accounts. Handled deployments through
-TestFlight.`,
-      `Worked at Opal, creating new product detail pages in a Shopify e-commerce using Liquid.`,
-    ],
-    img: "../src/assets/prototipo-3.png",
+    details: "",
+    img: "/images/prototipo-3.png",
     tags: ["Natural", "Cúpula cerrada"],
   },
 ];
