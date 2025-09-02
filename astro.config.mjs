b7c529bd28a99b2d997fdfe47404fe8b4f9a98bc
Miguel Amores <migueamores@gmail.com>
2025-09-01 20:30:52 -0500

    add vercel image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -11,4 +13,5 @@ export default defineConfig({
   },
 
   integrations: [react()],
+  adapter: vercel({ imageService: true }),
 });
